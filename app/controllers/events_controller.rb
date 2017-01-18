bc74543db682b15605e8559c3e3f58d9dd4b6510
Maxime <maxime.gauthier88@gmail.com>
2017-01-17 22:39:21 -0500

    feat: Display all pages events on first visit
    All pages events should be displayed (checked) on the first visit of a
    user.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,46 +1,42 @@
 class EventsController < ApplicationController
 
-  before_action :first_visit?
-
   before_action :page_ids, only: :index
 
-  helper_method :checked?
+  helper_method :checked?, :first_visit?
+
+  after_action :visited
 
   def index
     @events = Event.where(page_id: page_ids)
     @pages = Page.order(name: :asc)
-    @output = cookies.permanent[:first_visit?]
   end
 
   protected
 
     def default_url_options
-      if first_visit?
-        super.merge page_ids: Page.all.pluck(:id)
-      else
-        super.merge page_ids: page_ids
-      end
+      super.merge page_ids: page_ids
     end
 
   private
 
+    def first_visit?
+      return false if cookies[:visited?]
+      return true
+    end
+
+    def visited
+      cookies[:visited?] = true
+    end
+
     def page_ids
+      return Page.pluck(:id) if first_visit?
       params[:page_ids] || []
     end
 
     def checked?(id)
       return false if id.nil?
-      return true if page_ids.include? id.to_s
+      return true if page_ids.include? id.to_s || first_visit?
       false
-    end
-
-    def first_visit?
-      if cookies.permanent[:first_visit?] == nil
-        cookies.permanent[:first_visit?] = false
-        true
-      else
-        false
-      end
     end
 
 end
