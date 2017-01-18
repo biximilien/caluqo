97bbf0b1eb375c25ddb1a28af7756278b6d4e285
Maxime <maxime.gauthier88@gmail.com>
2017-01-17 22:54:46 -0500

    Remove first_visit? method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,5 @@
 class EventsController < ApplicationController
 
-  before_action :first_visit?
-
   before_action :page_ids, only: :index
 
   helper_method :checked?
@@ -9,17 +7,12 @@ class EventsController < ApplicationController
   def index
     @events = Event.where(page_id: page_ids)
     @pages = Page.order(name: :asc)
-    @output = cookies.permanent[:first_visit?]
   end
 
   protected
 
     def default_url_options
-      if first_visit?
-        super.merge page_ids: Page.all.pluck(:id)
-      else
-        super.merge page_ids: page_ids
-      end
+      super.merge page_ids: page_ids
     end
 
   private
@@ -32,15 +25,6 @@ class EventsController < ApplicationController
       return false if id.nil?
       return true if page_ids.include? id.to_s
       false
-    end
-
-    def first_visit?
-      if cookies.permanent[:first_visit?] == nil
-        cookies.permanent[:first_visit?] = false
-        true
-      else
-        false
-      end
     end
 
 end
