0dec1d037ef4effabe475d26f262df511170a828
Maxime <maxime.gauthier88@gmail.com>
2017-01-17 22:00:30 -0500

    feat: Add a helper method to check checkboxes
    Add a helper method to check the pages list checkboxes.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
 
   before_action :page_ids, only: :index
 
+  helper_method :checked?
+
   def index
     @events = Event.where(page_id: page_ids)
     @pages = Page.order(name: :asc)
@@ -10,10 +12,22 @@ class EventsController < ApplicationController
   def facebook
   end
 
+  protected
+
+    def default_url_options
+      { page_ids: page_ids }.merge(super)
+    end
+
   private
 
     def page_ids
-      params[:page_ids]
+      params[:page_ids] || []
+    end
+
+    def checked?(id)
+      return false if id.nil?
+      return true if page_ids.include? id.to_s
+      false
     end
 
 end
