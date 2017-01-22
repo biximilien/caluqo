92a13c9e1b29d8e11bfb0d029b56de3b07323495
Maxime <maxime.gauthier88@gmail.com>
2017-01-22 10:43:50 -0500

    Delegates import_all_events for Page to Facebook::Page.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,15 @@
+require 'facebook'
+
 class Page < ApplicationRecord
+
+  attr_accessor :facebook_page
+
+  after_initialize -> { self.facebook_page = Facebook::Page.new(self.facebook_id) }
+
   validates :facebook_id, uniqueness: true
 
   has_many :events
+
+  delegate :import_all_events, to: :facebook_page
+
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,7 @@
 describe Page, type: :model do
   it { should have_many :events }
-  
+
   it { should validate_uniqueness_of :facebook_id }
+
+  it { should delegate_method(:import_all_events).to(:facebook_page) }
 end
